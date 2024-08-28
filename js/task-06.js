cf04984f670f9d8e5cfcb6c507061b05de5a6cee
renaerdelyi <rena.szabo@yahoo.com>
2024-08-28 18:32:48 +0300

    modificare2

--- js/task-06.js
+++ js/task-06.js
@@ -4,9 +4,11 @@ validationInput.addEventListener("blur",()=>{
     const dataLength = Number(validationInput.dataset.length);
 
     if (validationInput.value.length === dataLength){
-        validationInput.classList.add("valid");
+    
+     validationInput.classList.add("valid");   validationInput.classList.remove("invalid");
     } else{
-        validationInput.classList.add("invalid");     
+        validationInput.classList.add("invalid"); 
+        validationInput.classList.remove("valid");    
     }
  console.log(dataLength);
 });
